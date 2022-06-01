8501c02e3a875bff8f0fd60bcd0ddecbd30bdc67
Aula Diena <16auladiena@gmail.com>
2022-06-01 10:19:21 +0800

    Buat getter and setter pengguna

--- src/model/Pengguna.java
+++ src/model/Pengguna.java
@@ -6,4 +6,55 @@ public class Pengguna {
     private String password;
     private String namaLengkap;
     private String level;
+
+    public Pengguna() {
+    }
+
+    public Pengguna(int id, String username, String password, String namaLengkap, String level) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.namaLengkap = namaLengkap;
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
 }
